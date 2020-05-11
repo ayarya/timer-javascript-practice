8371aa4dcc020485f1ec8b134f176b4a7b375a98
Ayush Arya <arya.ayush409@gmail.com>
2020-05-11 17:13:45 -0400

    Get and Set method for javascript class is created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class Timer {
 
 		this.startButton.addEventListener('click', this.start);
 		this.pauseButton.addEventListener('click', this.pause);
+		this.durationInput.addEventListener('click', this.pause);
 	}
 
 	start = () => {
@@ -14,8 +15,20 @@ class Timer {
 	};
 
 	tick = () => {
-		console.log('tick');
+		if (this.timeRemaining <= 0) {
+			this.pause();
+		} else {
+			this.timeRemaining = this.timeRemaining - 1;
+		}
 	};
+
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	set timeRemaining(time) {
+		this.durationInput.value = time;
+	}
 
 	pause = () => {
 		clearInterval(this.interval);

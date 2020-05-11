93de8f70b7e6dd19cfe1505acdfdc9db6a135977
Ayush Arya <arya.ayush409@gmail.com>
2020-05-11 17:21:33 -0400

    callbacks created

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 class Timer {
-	constructor(durationInput, startButton, pauseButton) {
+	constructor(durationInput, startButton, pauseButton, callbacks) {
 		this.durationInput = durationInput;
 		this.startButton = startButton;
 		this.pauseButton = pauseButton;
-
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
 		this.startButton.addEventListener('click', this.start);
 		this.pauseButton.addEventListener('click', this.pause);
 		this.durationInput.addEventListener('click', this.pause);
 	}
 
 	start = () => {
+		if (this.onStart) {
+			this.onStart();
+		}
 		this.tick();
 		this.interval = setInterval(this.tick, 1000);
 	};
@@ -17,8 +24,14 @@ class Timer {
 	tick = () => {
 		if (this.timeRemaining <= 0) {
 			this.pause();
+			if (this.onComplete) {
+				this.onComplete();
+			}
 		} else {
 			this.timeRemaining = this.timeRemaining - 1;
+			if (this.onTick) {
+				this.onTick();
+			}
 		}
 	};
 
@@ -39,4 +52,8 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart() {},
+	onTick() {},
+	onComplete() {}
+});
